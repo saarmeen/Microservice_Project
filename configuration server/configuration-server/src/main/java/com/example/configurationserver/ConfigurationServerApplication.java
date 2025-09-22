925d73b3b010b2853683a12ef475148954e0176b
saarmeen <saarmeenfarooqui@gmail.com>
2025-09-22 11:27:20 +0530

    Config server added

---
+++ configuration server/configuration-server/src/main/java/com/example/configurationserver/ConfigurationServerApplication.java
@@ -0,0 +1,24 @@
+package com.example.configurationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigurationServerApplication {
+
+	public static void main(String[] args) {
+		String mode = System.getenv("CONFIG_MODE");
+
+		SpringApplication app = new SpringApplication(ConfigurationServerApplication.class);
+		if("native".equalsIgnoreCase(mode)){
+			app.setAdditionalProfiles("native");
+		}else{
+			app.setAdditionalProfiles("git");
+		}
+		app.run(args);
+
+	}
+
+}
